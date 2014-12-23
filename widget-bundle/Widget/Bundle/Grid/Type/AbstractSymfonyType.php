5d466afb15f5b0a06fb594d30f0e50e2139b39b6
igdr <igdr@ciklum.com>
2014-12-23 10:23:39 +0200

    fix bug with symfony 2.6 with translator

--- widget-bundle/Widget/Bundle/Grid/Type/AbstractSymfonyType.php
+++ widget-bundle/Widget/Bundle/Grid/Type/AbstractSymfonyType.php
@@ -2,7 +2,7 @@
 namespace Widget\Bundle\Grid\Type;
 
 use Symfony\Bundle\FrameworkBundle\Translation\Translator;
-use Widget\Grid\AbstractGridCreator;
+use Symfony\Component\Translation\TranslatorInterface;
 use Widget\Grid\AbstractType;
 
 /**
@@ -11,14 +11,14 @@ use Widget\Grid\AbstractType;
 abstract class AbstractSymfonyType extends AbstractType
 {
     /**
-     * @var Translator
+     * @var TranslatorInterface
      */
     protected $translator;
 
     /**
-     * @param Translator $translator
+     * @param TranslatorInterface $translator
      */
-    public function setTranslator(Translator $translator)
+    public function setTranslator(TranslatorInterface $translator)
     {
         $this->translator = $translator;
     }
